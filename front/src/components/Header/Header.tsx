09f66163d1982de41644f22dc844de13a79dc0b9
Jhon Miguel Rodríguez <130907039+jhonmirh@users.noreply.github.com>
2025-01-31 07:37:07 -0400

    nav header

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -12,12 +12,12 @@ const Header = () => {
   const { userData } = useLoggin();
 
   return (
-    <header className="sticky top-0 w-full h-[4.5rem] bg-grisOscuro text-white z-50 flex justify-between items-center px-6">
+    <header className="sticky top-0 w-full h-[10rem] bg-grisOscuro text-white z-50 flex justify-between items-center px-6">
       <div className="flex items-center">
         <Link href="/">
           <Image
             src="/HE.png"
-            alt="Elysim"
+            alt="ETIMA"
             width={120}
             height={50}
             className="hover:scale-105 transition-transform duration-200"
@@ -26,55 +26,55 @@ const Header = () => {
 
         <ContadorVisitas />
       </div>
-      <nav className="flex items-center space-x-6 text-[1rem] uppercase">
-        <div className="flex space-x-6 text-[1rem] uppercase items-center">
-          {userData?.userData.role.name === "Administrador" ? (
-            <>
-              <Link href="/clientlist" className="hover:text-mostaza">
-                Historial de Clientes
-              </Link>
-              <Link href="/roomsManagement" className="hover:text-mostaza">
-                Registro de Habitaciones
-              </Link>
-              <Link href="/employee" className="hover:text-mostaza">
-                Crear Empleados
-              </Link>
-              <Link href="/testimonialsadmin" className="hover:text-mostaza">
-                Aprobar Testimonios
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link href="/" className="hover:text-mostaza">
-                Home
-              </Link>
-              <Link href="/rooms" className="hover:text-mostaza">
-                Habitaciones
-              </Link>
-              <Link href="/facilities" className="hover:text-mostaza">
-                Servicios
-              </Link>
-              <Link href="/contact" className="hover:text-mostaza">
-                Contacto
-              </Link>
-              <Link href="/nosotros" className="hover:text-mostaza">
-                Acerca de...
-              </Link>
-            </>
-          )}
-          <Switcher />
-        </div>
-      </nav>
+      <nav className="flex items-center justify-center text-[1rem] uppercase">
+  <div className="flex flex-wrap gap-6 items-center">
+    {userData?.userData.role.name === "Administrador" ? (
+      <>
+        <Link href="/clientlist" className="hover:text-mostaza text-center block">
+          Historial <br /> de Clientes
+        </Link>
+        <Link href="/roomsManagement" className="hover:text-mostaza text-center block">
+          Registro <br /> de Habitaciones
+        </Link>
+        <Link href="/employee" className="hover:text-mostaza text-center block">
+          Crear <br /> Empleados
+        </Link>
+        <Link href="/testimonialsadmin" className="hover:text-mostaza text-center block">
+          Aprobar <br /> Testimonios
+        </Link>
+      </>
+    ) : (
+      <>
+        <Link href="/" className="hover:text-mostaza text-center block">
+          Home
+        </Link>
+        <Link href="/rooms" className="hover:text-mostaza text-center block">
+          Oportunidades <br /> de Estudio
+        </Link>
+        <Link href="/facilities" className="hover:text-mostaza text-center block">
+          Servicios <br /> Estudiantiles
+        </Link>
+        <Link href="/contact" className="hover:text-mostaza text-center block">
+          Contacto
+        </Link>
+        <Link href="/nosotros" className="hover:text-mostaza text-center block">
+          Acerca <br /> de...
+        </Link>
+      </>
+    )}
+  </div>
+</nav>
+
       <div className="space-x-4 flex items-center">
         {userData?.token ? (
           <CardUser />
         ) : (
           <>
             <Link href="/login">
-              <button>Iniciar Sesión</button>
+              <button className="bg-mostaza hover:bg-grisClaro">Iniciar Sesión</button>
             </Link>
             <Link href="/register">
-              <button>Registrarse</button>
+              <button className="bg-mostaza hover:bg-grisClaro">Registrarse</button>
             </Link>
           </>
         )}
